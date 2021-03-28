e827b53570784a60d73d8bad76882cf97370b8a8
Guilherme de Matos <guilherme.dmatos@hotmail.com>
2021-03-28 11:33:44 -0300

    init

---
+++ app/src/main/java/com/aeroshi/bitcoinprice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aeroshi.bitcoinprice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
